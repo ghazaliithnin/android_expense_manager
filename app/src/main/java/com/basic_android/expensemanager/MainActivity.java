531d8006c18b2bcdaccfbb962fe1d090fcb4cbba
ghazaliithnin <ghazali6013@gmail.com>
2021-09-16 11:25:06 +0800

    First expense manager commit

---
+++ app/src/main/java/com/basic_android/expensemanager/MainActivity.java
@@ -0,0 +1,18 @@
+package com.basic_android.expensemanager;
+
+
+// Tutorial from @beingknow  youtube, Expense Manager App
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
